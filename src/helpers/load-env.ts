8968c89aa8545ec2568d2927fe4bc2774b8bc66d
Artem Prokop <44642024+ExposedCat@users.noreply.github.com>
2023-10-08 23:34:33 +0200

    Initial commit

---
+++ src/helpers/load-env.ts
@@ -0,0 +1,9 @@
+import dotenv from 'dotenv'
+import { resolvePath } from './resolve-path.js'
+
+export function loadEnv(configPath = '.env') {
+	const fullPath = resolvePath(import.meta.url, configPath)
+	dotenv.config({
+		path: fullPath
+	})
+}
---
+++ src/services/user.ts
@@ -0,0 +1,38 @@
+import type { Database, User } from '../types/database.js'
+
+export function buildName(firstName: string, lastName?: string) {
+	return lastName ? `${firstName} ${lastName}` : firstName
+}
+
+async function createUser(args: {
+	db: Database
+	userId: number
+	name: string
+}): Promise<User> {
+	const userObject = {
+		userId: args.userId,
+		name: args.name
+	} as User
+
+	await args.db.user.insertOne(userObject)
+
+	return userObject
+}
+
+export async function getOrCreatePlayer(args: {
+	db: Database
+	userId: number
+	name: string
+}): Promise<User> {
+	const user = await args.db.user.findOneAndUpdate(
+		{ userId: args.userId },
+		{ $set: { name: args.name } },
+		{ returnDocument: 'after' }
+	)
+
+	if (user.ok && user.value) {
+		return user.value
+	}
+
+	return createUser(args)
+}
