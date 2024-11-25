767f6c04419a8c6bdd5dd24605d762084ee0900a
Artem Prokop <artem13.prokop@gmail.com>
2024-11-25 16:57:14 +0100

    fix: audio

--- src/controllers/yt-audio-download.ts
+++ src/controllers/yt-audio-download.ts
@@ -6,15 +6,16 @@ import { deleteFile } from '../helpers/fs.js';
 
 export const ytAudioDownloadController = new Composer<CustomContext>();
 
-ytAudioDownloadController.chatType(['supergroup', 'private', 'group']).hears(/\/mp3 (.+)/, async (ctx, next) => {
+ytAudioDownloadController.chatType(['supergroup', 'private', 'group']).command('mp3', async (ctx, next) => {
   const url = ctx.match.at(1);
   if (!url) {
     await next();
     return;
   }
 
-  const filepath = `/tmp/ummrobot-${Date.now()}-${ctx.from.id}.mp4`;
+  const filepath = `/tmp/ummrobot-${Date.now()}-${ctx.from.id}.mp3`;
   try {
+    await ctx.text('status.downloading');
     const filename = await downloadYouTubeAudio(ctx.binary, url, filepath);
     await ctx.replyWithAudio(new InputFile(filename));
     await deleteFile(filename);
